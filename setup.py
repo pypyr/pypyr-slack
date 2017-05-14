e696722c5bbb9213a26ca57c16d42e32531a31f8
345thomas <thomas@345.systems>
2017-05-14 19:06:49 +0100

    add pypyr to keywords

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
     ],
 
     # What does your project relate to?
-    keywords='devops pipeline runner slack plug-in integration',
+    keywords='pypyr slack plugin devops pipeline runner',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
