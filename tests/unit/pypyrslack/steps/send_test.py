58fc726e13abbe4cc95f02fc31190c49c4dd5621
yaythomas <18520168+yaythomas@users.noreply.github.com>
2019-08-18 03:46:47 +0100

    update slack api to v2.x

--- tests/unit/pypyrslack/steps/send_test.py
+++ tests/unit/pypyrslack/steps/send_test.py
@@ -4,12 +4,12 @@ from pypyr.context import Context
 from pypyr.errors import KeyInContextHasNoValueError, KeyNotInContextError
 from pypyrslack.errors import SlackSendError
 from pypyrslack.steps import send
-from slackclient import SlackClient
+from slack import WebClient
 from unittest.mock import patch
 
 
-@patch.object(SlackClient, 'api_call')
-@patch.object(SlackClient, '__init__', return_value=None)
+@patch.object(WebClient, 'chat_postMessage')
+@patch.object(WebClient, '__init__', return_value=None)
 def test_send_pass(mock_client, mock_post):
     """Success case for slack send."""
     mock_post.return_value = {'ok': True}
@@ -20,14 +20,13 @@ def test_send_pass(mock_client, mock_post):
 
     mock_client.assert_called_once_with('in your dreams')
     mock_post.assert_called_once_with(
-        'chat.postMessage',
         as_user=True,
         channel='#blah',
         text='this is :boom: text')
 
 
-@patch.object(SlackClient, 'api_call')
-@patch.object(SlackClient, '__init__', return_value=None)
+@patch.object(WebClient, 'chat_postMessage')
+@patch.object(WebClient, '__init__', return_value=None)
 def test_send_with_string_interpolation(mock_client, mock_post):
     """Slack send with string interpolation"""
     mock_post.return_value = {'ok': True}
@@ -42,14 +41,13 @@ def test_send_with_string_interpolation(mock_client, mock_post):
 
     mock_client.assert_called_once_with('in your dreams')
     mock_post.assert_called_once_with(
-        'chat.postMessage',
         as_user=True,
         channel='#channelarb',
         text='arb value2 this is :boom: text arb value1')
 
 
-@patch.object(SlackClient, 'api_call')
-@patch.object(SlackClient, '__init__', return_value=None)
+@patch.object(WebClient, 'chat_postMessage')
+@patch.object(WebClient, '__init__', return_value=None)
 def test_slack_error_raises(mock_client, mock_post):
     """SlackSendError on slack failure."""
     # Actual error example: {'ok': False, 'error': 'channel_not_found'}
